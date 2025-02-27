e4d9048f03d80845d492a5b66b6bdd6412650583
Turazzi <analivia.turazzi@gmail.com>
2025-02-27 17:33:06 -0300

    remoção de código desnecessário

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -66,14 +66,6 @@ class MainActivity : AppCompatActivity() {
         buttonLimpar.setOnClickListener {
             limparFormulario()
         }
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-
-        }
     }
 
     private fun formularioValido(): Boolean {
